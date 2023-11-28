2eafad47f2f501c76b7f2e5b44ad27f8a92dc204
Giovanni Brignone <brigio345@gmail.com>
2023-11-28 00:14:28 +0000

    chore: add scripts for LLVM 3.1 install and out-of-tree pass build

---
+++ install_llvm.sh
@@ -0,0 +1,13 @@
+mkdir -p llvm
+cd llvm
+wget https://releases.llvm.org/3.1/llvm-3.1.src.tar.gz
+tar -xf llvm-3.1.src.tar.gz
+wget https://releases.llvm.org/3.1/clang-3.1.src.tar.gz
+tar -xf clang-3.1.src.tar.gz
+mv clang-3.1.src llvm-3.1.src/tools/clang
+mkdir -p build
+mkdir -p install
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../install ../llvm-3.1.src
+make install -j 8
+

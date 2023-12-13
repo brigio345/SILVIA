cd34a938b9b6d22f14f22279f2e2a50e45e252c1
Fabrizio Ottati <fabriziottati@gmail.com>
2023-12-13 21:03:56 +0000

    Formatting.

--- blackbox/giovanni/giovanni.h
+++ blackbox/giovanni/giovanni.h
@@ -3,7 +3,7 @@
 
 #include <ap_int.h>
 
-ap_int<48> giovanni(ap_int<24> a0=0, ap_int<24> a1=0, ap_int<24> b0=0,
-                               ap_int<24> b1=0);
+ap_int<48> giovanni(ap_int<24> a0 = 0, ap_int<24> a1 = 0, ap_int<24> b0 = 0,
+                    ap_int<24> b1 = 0);
 
 #endif

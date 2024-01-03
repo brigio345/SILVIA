9bce3d85de7a805b1bd98ca39d98e29f961c5a54
Giovanni Brignone <brigio345@gmail.com>
2024-01-03 10:14:11 +0000

    fix(scripts): avoid using absolute paths

--- scripts/csynth_design_simd.tcl
+++ scripts/csynth_design_simd.tcl
@@ -1,22 +1,27 @@
 ::common::set_param hls.enable_hidden_option_error false
 
-proc csynth_design_simd {solution_path llvm_install_path {blackbox_root ".."}} {
+proc csynth_design_simd {solution_path llvm_install_path {simd_root ".."}} {
+	if { ![info exists ::env(HLS_LLVM_PLUGIN_DIR)] } {
+		set ::env(HLS_LLVM_PLUGIN_DIR) [file normalize ../../llvm-3.9/lib/InsertDummyBB]
+	}
+
 	if { ![file exists $::env(HLS_LLVM_PLUGIN_DIR)/LLVMCustomPasses.so] } {
 		error "Must build LLVMCustomPasses.so before running this example"
 	}
 
+	set ::SIMD_ROOT ${simd_root}
 	set ::LLVM_CUSTOM_LINK \
 		$::env(XILINX_HLS)/lnx64/tools/clang-3.9-csynth/bin/llvm-link
 	set ::LLVM_CUSTOM_NM \
 		$::env(XILINX_HLS)/lnx64/tools/clang-3.9-csynth/bin/llvm-nm
 	set ::BB_BC ${solution_path}/.autopilot/db/add4simd.g.bc
 
-	set ::LLVM_CUSTOM_CMD {$::env(HOME)/llvm/hls-llvm-simd/scripts/safe_link.sh \
+	set ::LLVM_CUSTOM_CMD {$::SIMD_ROOT/scripts/safe_link.sh \
 		$::LLVM_CUSTOM_NM $::LLVM_CUSTOM_LINK $LLVM_CUSTOM_OPT \
 		$LLVM_CUSTOM_INPUT $LLVM_CUSTOM_OUTPUT $::env(HLS_LLVM_PLUGIN_DIR) $::BB_BC\
 	}
 
-	exec ln -sf ${blackbox_root}/blackbox .
+	exec ln -sf ${simd_root}/blackbox .
 	add_files -blackbox blackbox/add4simd/add4simd.json
 	csynth_design
 	exec unzip -o -d dut ${solution_path}/.autopilot/db/dut.hcp
--- test/add_ap_int_12/run_hls.tcl
+++ test/add_ap_int_12/run_hls.tcl
@@ -14,12 +14,8 @@ open_solution -reset solution1
 set_part "xczu3eg-ubva530-2L-e"
 
 create_clock -period "300MHz"
-if { ![info exists ::env(HLS_LLVM_PLUGIN_DIR)] } {
-    # Use plugin example directory as default build directory
-  set ::env(HLS_LLVM_PLUGIN_DIR) [file normalize ../../llvm-3.9/lib/InsertDummyBB]
-}
-csynth_design_simd ${PROJ_NAME}/solution1 \
-  $::env(HOME)/llvm/hls-llvm-simd/llvm-3.1/llvm/install ../..
+
+csynth_design_simd ${PROJ_NAME}/solution1 ../../llvm-3.1/llvm/install ../..
 
 cosim_design
 

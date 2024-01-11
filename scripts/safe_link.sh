2eeeed8384fbb4ddfe259ec3ba70334cba8d5e8f
Giovanni Brignone <brigio345@gmail.com>
2024-01-11 22:50:07 +0000

    chore(scripts): adapt to generalized InsertDummyBB

--- scripts/safe_link.sh
+++ scripts/safe_link.sh
@@ -6,16 +6,19 @@ LLVM_OPT=$3
 LLVM_INPUT=$4
 LLVM_OUTPUT=$5
 LLVM_PASS_DIR=$6
-BB_BC=$7
+SOLUTION_DIR=$7
+BB_NAME=$8
+
+BB_BC=$SOLUTION_DIR/.autopilot/db/$BB_NAME.g.bc
 
 $LLVM_NM $LLVM_INPUT | grep giovanni > /dev/null
 if [ ! $? -eq 0 ]; then
   $LLVM_LINK $LLVM_INPUT $BB_BC -o - | \
     $LLVM_OPT -mem2reg -load \
     $LLVM_PASS_DIR/LLVMCustomPasses.so -insert_dummy_bb \
-    - -o $LLVM_OUTPUT
+    -insert-dummy-bb-fn $BB_NAME - -o $LLVM_OUTPUT
 else 
   $LLVM_OPT -mem2reg -load \
   $LLVM_PASS_DIR/LLVMCustomPasses.so -insert_dummy_bb \
-  $LLVM_INPUT -o $LLVM_OUTPUT
+  -insert-dummy-bb-fn $BB_NAME $LLVM_INPUT -o $LLVM_OUTPUT
 fi

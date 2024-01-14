cc25eb0d04812eccf420aab48ac4cb8d13e4ae92
Fabrizio Ottati <fabriziottati@gmail.com>
2024-01-14 15:17:54 +0000

    feat(dotprod): New test and Versal device
    - Added Versal device to synthesis script.
    - Matrix-vector multiplication example.

---
+++ test/mac-int8/run_hls.tcl
@@ -0,0 +1,24 @@
+source ../../scripts/csynth_design_simd.tcl
+
+set PROJ_NAME "proj"
+set VERSAL "xcvc1902-vsva2197-2MP-e-S"
+
+open_project -reset ${PROJ_NAME}
+
+add_files test.cpp
+add_files -tb test.cpp
+
+set_top example
+
+open_solution -reset solution1
+
+# set_part "xczu3eg-ubva530-2L-e"
+set_part $VERSAL
+
+create_clock -period "300MHz"
+
+csynth_design_simd dotprod ${PROJ_NAME}/solution1 ../../llvm-3.1/llvm/install ../..
+
+cosim_design
+
+exit

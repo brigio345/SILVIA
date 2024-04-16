4ffece3a73855abb5be218e6ce77f990ba2346f7
Giovanni Brignone <brigio345@gmail.com>
2024-04-16 09:17:38 +0000

    fix(scripts:csynth_design_simd): create a temporary solution for the first csynth
    This change improves the robustness of the SIMD workflow.

--- scripts/csynth_design_simd.tcl
+++ scripts/csynth_design_simd.tcl
@@ -1,20 +1,29 @@
+package require tdom
+
 ::common::set_param hls.enable_hidden_option_error false
 
-proc csynth_design_simd {simd_op simd_factor solution_path llvm_install_path {simd_root ".."}} {
-	if { ![info exists ::env(HLS_LLVM_PLUGIN_DIR)] } {
-		set ::env(HLS_LLVM_PLUGIN_DIR) [file normalize ../../llvm-3.9/lib/CallBlackBox]
-	}
-
-	if { ![file exists $::env(HLS_LLVM_PLUGIN_DIR)/LLVMCustomPasses.so] } {
-		error "Must build LLVMCustomPasses.so before running this example"
-	}
-
+proc csynth_design_simd {simd_op simd_factor llvm_install_path {simd_root ".."}} {
+	set project_path [get_project -directory]
+	set solution_name [get_solution]
+	file copy -force -- ${project_path}/${solution_name} ${project_path}/${solution_name}_FE
+	file rename ${project_path}/${solution_name}_FE/${solution_name}.aps ${project_path}/${solution_name}_FE/${solution_name}_FE.aps
+	set aps_file [open ${project_path}/${solution_name}_FE/${solution_name}_FE.aps r]
+	set doc [dom parse [read ${aps_file}]]
+	close ${aps_file}
+	set root [${doc} documentElement]
+	set solution_name_node [${root} selectNode "/AutoPilot:solution/name/value"]
+	${solution_name_node} setAttribute string ${solution_name}_FE
+	set aps_file [open ${project_path}/${solution_name}_FE/${solution_name}_FE.aps w]
+	puts ${aps_file} [${doc} asXML]
+	close ${aps_file}
+	open_solution ${solution_name}_FE
 	csynth_design
-	exec unzip -o -d dut ${solution_path}/.autopilot/db/dut.hcp
+	exec unzip -o -d dut ${project_path}/${solution_name}_FE/.autopilot/db/dut.hcp
 	set ::env(LD_LIBRARY_PATH) "${llvm_install_path}/lib/:$::env(LD_LIBRARY_PATH)"
 	exec ${llvm_install_path}/bin/llvm-link ${simd_root}/template/${simd_op}/${simd_op}.ll dut/a.o.3.bc -o dut/a.o.3.bc
 	exec ${llvm_install_path}/bin/opt -load ${llvm_install_path}/lib/LLVMSIMDAdd.so -basicaa -simd-add -simd-add-op ${simd_op} -simd-add-factor ${simd_factor} -dce dut/a.o.3.bc -o dut/a.o.3.bc |& cat | tee SIMDAdd.log
 	exec zip -rj dut.hcp dut
+	open_solution ${solution_name}
 	read_checkpoint dut.hcp
 	csynth_design -hw_syn
 

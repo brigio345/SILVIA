a52feb7a5f1be4ed9f2e592e4615be2061263426
Giovanni Brignone <brigio345@gmail.com>
2024-04-16 16:58:19 +0000

    cleanup: remove unused blackbox files

--- blackbox/dotprod/dotprod.cpp
+++
@@ -1,7 +0,0 @@
-#include "dotprod.h"
-
-ap_int<58> dotprod(ap_int<8> a0, ap_int<8> b0, ap_int<8> a1,
-		ap_int<8> b1, ap_int<8> a2, ap_int<8> b2) {
-#pragma HLS inline off
-  return ((a0 * b0) + (a1 * b1) + (a2 * b2));
-}
--- blackbox/dotprod/dotprod.h
+++
@@ -1,9 +0,0 @@
-#ifndef DOTPROD_H_
-#define DOTPROD_H_
-
-#include <ap_int.h>
-
-ap_int<58> dotprod(ap_int<8> a0, ap_int<8> b0, ap_int<8> a1,
-		ap_int<8> b1, ap_int<8> a2, ap_int<8> b2);
-
-#endif  // DOTPROD_H_

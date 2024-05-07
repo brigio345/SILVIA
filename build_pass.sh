5614873b2971410a6100ec2a81738cca21feaee7
Giovanni Brignone <brigio345@gmail.com>
2024-05-07 11:00:01 +0000

    fix: make build scripts more platform-independent

--- build_pass.sh
+++ build_pass.sh
@@ -3,7 +3,7 @@ LLVM_INSTALL_PATH=`pwd`/llvm-project/install
 pushd `pwd`
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${LLVM_INSTALL_PATH} ../lib
+${XILINX_VITIS}/tps/lnx64/cmake-3.24.2/bin/cmake -DCMAKE_INSTALL_PREFIX=${LLVM_INSTALL_PATH} ../lib
 make install
 popd
 
